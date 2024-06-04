638381de4e2c155ee33448e5a2d7ee3ba3a3ea5e
kang yi fan <jason3631061@gmail.com>
2024-06-03 23:01:08 -0700

    Add GetWeatherByCity service

--- WeatherApp/Services/ApiService.cs
+++ WeatherApp/Services/ApiService.cs
@@ -9,13 +9,23 @@ using WeatherApp.Models;
 
 namespace WeatherApp.Services;
 
-public class ApiService
+public static class ApiService
 {
-    public async Task<Root> GetWeather(double latitude, double longitude)
+    public static async Task<Root> GetWeather(double latitude, double longitude)
     {
         string apiUrl = string.Format("http://api.openweathermap.org/data/2.5/forecast?lat={0}&lon={1}&appid=b1a40fac2d8a3e9484e57de36c664fd0", latitude, longitude);
         
         HttpClient httpClient = new();        
+        string response = await httpClient.GetStringAsync(apiUrl);
+
+        return JsonConvert.DeserializeObject<Root>(response);
+    }
+
+    public static async Task<Root> GetWeatherByCity(string cityName)
+    {
+        string apiUrl = string.Format("api.openweathermap.org/data/2.5/forecast?q={0}&appid=b1a40fac2d8a3e9484e57de36c664fd0", cityName);
+
+        HttpClient httpClient = new();
         string response = await httpClient.GetStringAsync(apiUrl);
 
         return JsonConvert.DeserializeObject<Root>(response);

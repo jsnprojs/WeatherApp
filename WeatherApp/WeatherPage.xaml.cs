5baf455ffde4a490ea3f22f8cded6b0d8974bde1
kang yi fan <jason3631061@gmail.com>
2024-06-06 03:17:35 -0700

    Add display for city name, description and temperature

--- WeatherApp/WeatherPage.xaml.cs
+++ WeatherApp/WeatherPage.xaml.cs
@@ -1,3 +1,5 @@
+using WeatherApp.Services;
+
 namespace WeatherApp;
 
 public partial class WeatherPage : ContentPage
@@ -5,5 +7,18 @@ public partial class WeatherPage : ContentPage
 	public WeatherPage()
 	{
 		InitializeComponent();
-	}
+
+    }
+
+    protected async override void OnAppearing()
+    {
+        base.OnAppearing();
+
+        var result = await ApiService.GetWeatherByCity("Elk Grove");
+
+        LblCity.Text = result.city.name;
+        LblWeatherDescription.Text = result.list[0].weather[0].description;
+
+        LblTemperature.Text = result.list[0].main.temp + "°C";
+    }
 }
